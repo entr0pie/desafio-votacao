30530b4ed55e8a38d06028e86d9c65409ac982a2
tandera <tandera@notemail.com>
2024-06-16 00:36:46 -0300

    feat: add agenda endpoints

---
+++ src/test/java/db/server/desafio_votacao/domain/agenda/controllers/AgendaControllerAdviceTest.java
@@ -0,0 +1,36 @@
+package db.server.desafio_votacao.domain.agenda.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import db.server.desafio_votacao.domain.agenda.exceptions.AgendaNotFoundException;
+import db.server.desafio_votacao.domain.common.dtos.ExpectedErrorMessage;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
+public class AgendaControllerAdviceTest {
+
+	@Autowired
+	private AgendaControllerAdvice agendaControllerAdvice;
+
+	@Test
+	@DisplayName("Should handle AgendaNotFoundException and return 404 status code")
+	public void testHandleHandleNotFound() {
+		String expectedMessage = "Agenda not found";
+		AgendaNotFoundException ex = new AgendaNotFoundException(expectedMessage);
+
+		ResponseEntity<ExpectedErrorMessage> message = agendaControllerAdvice.handleHandleNotFound(ex);
+
+		assertEquals(HttpStatus.NOT_FOUND, message.getStatusCode(), "Should return 404 status code.");
+		assertEquals(expectedMessage, message.getBody().getError(), "Should return expected message.");
+	}
+}
