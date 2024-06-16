8f3cdab287d4af51bc1f7973c8cc1864cf2e4696
tandera <tandera@notemail.com>
2024-06-16 00:34:32 -0300

    docs: swagger definition of app on interface

---
+++ src/main/java/db/server/desafio_votacao/DesafioVotacaoSwagger.java
@@ -0,0 +1,13 @@
+package db.server.desafio_votacao;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+/**
+ * Basic OpenAPI definition for the Desafio Votacao application.
+ * 
+ * @author Caio Porcel
+ */
+@OpenAPIDefinition(info = @Info(title = "${info.app.name}", description = "${info.app.description}", version = "${info.app.version}"))
+public interface DesafioVotacaoSwagger {
+}
