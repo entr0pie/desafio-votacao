22317642773d854ff0ee0346ea70d6ec605d212c
tandera <tandera@notemail.com>
2024-06-16 00:35:14 -0300

    fix: add dev profile and test configuration

---
+++ src/main/java/db/server/desafio_votacao/config/ObjectMapperConfiguration.java
@@ -0,0 +1,22 @@
+package db.server.desafio_votacao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Configuration for jackson's {@link ObjectMapper}.
+ * 
+ * @author Caio Porcel
+ */
+@Configuration
+public class ObjectMapperConfiguration {
+
+	@Bean
+	@Profile("test")
+	ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
+}
