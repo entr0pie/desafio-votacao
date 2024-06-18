1abcbbad951d5c5c706a4b4891bfbf2cefba879d
tandera <tandera@notemail.com>
2024-06-18 00:59:01 -0300

    feat: user service and repository test

--- src/test/java/db/server/desafio_votacao/domain/agenda/repositories/UserRepositoryTest.java
+++ src/test/java/db/server/desafio_votacao/domain/agenda/repositories/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package db.server.desafio_votacao.domain.agenda.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
@@ -24,21 +25,31 @@ public class UserRepositoryTest {
 	@DisplayName("Should find by email")
 	public void shouldFindByEmail() {
 		String email = "email";
-		UserModel expectedUser = new UserModel(1, email, "cpf");
+		UserModel expectedUser = new UserModel(1, email, "cpf", null);
 		this.userRepository.save(expectedUser);
 
-		assertEquals(expectedUser, this.userRepository.findByEmail(email).get(),
-				"Find by email must return the expected user.");
+		UserModel receivedUser = this.userRepository.findByEmail(email).get();
+
+		assertAll("Find by email must return the expected user.", () -> {
+			assertEquals(expectedUser.getId(), receivedUser.getId(), "Id must be the same.");
+			assertEquals(expectedUser.getEmail(), receivedUser.getEmail(), "Email must be the same.");
+			assertEquals(expectedUser.getCpf(), receivedUser.getCpf(), "Cpf must be the same.");
+		});
 	}
 
 	@Test
 	@DisplayName("Should find by cpf")
 	public void shouldFindByCpf() {
 		String cpf = "cpf";
-		UserModel expectedUser = new UserModel(1, "email", cpf);
+		UserModel expectedUser = new UserModel(1, "email", cpf, null);
 		this.userRepository.save(expectedUser);
 
-		assertEquals(expectedUser, this.userRepository.findByCpf(cpf).get(),
-				"Find by cpf must return the expected user.");
+		UserModel receivedUser = this.userRepository.findByCpf(cpf).get();
+
+		assertAll("Find by cpf must return the expected user.", () -> {
+			assertEquals(expectedUser.getId(), receivedUser.getId(), "Id must be the same.");
+			assertEquals(expectedUser.getEmail(), receivedUser.getEmail(), "Email must be the same.");
+			assertEquals(expectedUser.getCpf(), receivedUser.getCpf(), "Cpf must be the same.");
+		});
 	}
 }
--- src/test/java/db/server/desafio_votacao/domain/user/services/UserServiceJpaTest.java
+++ src/test/java/db/server/desafio_votacao/domain/user/services/UserServiceJpaTest.java
@@ -16,7 +16,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-import db.server.desafio_votacao.domain.user.exceptions.UserAlreadyRegistered;
+import db.server.desafio_votacao.domain.user.exceptions.UserAlreadyRegisteredException;
+import db.server.desafio_votacao.domain.user.exceptions.UserNotFoundException;
 import db.server.desafio_votacao.domain.user.models.UserModel;
 import db.server.desafio_votacao.domain.user.repositories.UserRepository;
 
@@ -39,7 +40,7 @@ public class UserServiceJpaTest {
 		when(this.userRepository.findByEmail(eq("email"))).thenReturn(Optional.of(user));
 		when(this.userRepository.findByCpf(eq("cpf"))).thenReturn(Optional.empty());
 
-		assertThrows(UserAlreadyRegistered.class, () -> {
+		assertThrows(UserAlreadyRegisteredException.class, () -> {
 			this.userServiceJpa.register("email", "cpf");
 		}, "The service must throw AlreadyRegisteredUser when the email is already registered.");
 	};
@@ -52,7 +53,7 @@ public class UserServiceJpaTest {
 		when(this.userRepository.findByEmail(eq("email"))).thenReturn(Optional.empty());
 		when(this.userRepository.findByCpf(eq("cpf"))).thenReturn(Optional.of(user));
 
-		assertThrows(UserAlreadyRegistered.class, () -> {
+		assertThrows(UserAlreadyRegisteredException.class, () -> {
 			this.userServiceJpa.register("email", "cpf");
 		}, "The service must throw AlreadyRegisteredUser when the cpf is already registered.");
 	};
@@ -72,5 +73,25 @@ public class UserServiceJpaTest {
 			assertEquals(user, result, "The return value must be the repository's return.");
 		});
 	};
+
+	@Test
+	@DisplayName("Should throw UserNotFoundException when get by id")
+	public void shouldThrowUserNotFoundException_whenGetById() {
+		when(this.userRepository.findById(eq(1))).thenReturn(Optional.empty());
+		assertThrows(UserNotFoundException.class, () -> {
+			this.userServiceJpa.findById(1);
+		}, "The service must throw UserNotFoundException when the user is not found.");
+	}
+
+	@Test
+	@DisplayName("Should get user by id")
+	public void shouldGetUserById() {
+		UserModel user = new UserModel(1, "email", "cpf");
+		when(this.userRepository.findById(eq(1))).thenReturn(Optional.of(user));
+		assertDoesNotThrow(() -> {
+			UserModel result = this.userServiceJpa.findById(1);
+			assertEquals(user, result, "The return value must be the repository's return.");
+		});
+	}
 
 }
